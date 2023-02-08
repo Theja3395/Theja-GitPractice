1fc65ce2238426b16bde32942c98d8fdaf3916c7
LENOVO <LENOVO@DESKTOP-L0CE2LE>
2023-02-08 20:01:52 +0530

    commit by theja

---
+++ src/test/java/demo/luckyTest.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class luckyTest {
+	@Test
+ public void luckyTest1() {
+	 System.out.println("lucky--1");
+ }
+ @Test
+ public void luckyTest2() {
+	 System.out.println("lucky--2");
+ }
+}
